01aca67fed9a2fe2b6b2e2a64d6ffb594d0da2f2
Paulo <32718867+paulocezargit@users.noreply.github.com>
2025-05-20 15:40:00 -0300

    Creating function to calculate
    This time, we started creating the initial structures of the panel where the calculator will display the result. We also created the JavaScript function that will be responsible for performing the calculation based on the values entered by the user.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-//Script js
+function calculateBMI(weightKg, heightMeters) {
+
+	if (weightKg <= 0 || heightMeters <= 0) {
+
+		//return "Invalid input. Weight and height must be greater than zero.";
+
+	}
+
+	const bmi = weightKg / (heightMeters * heightMeters);
+	let category = "";
+
+	if (bmi < 18.5) {
+
+		category = "Underweight";
+
+	} else if (bmi < 24.9) {
+
+		category = "Normal weight";
+
+	} else if (bmi < 29.9) {
+
+		category = "Overweight";
+
+	} else {
+
+		category = "Obesity";
+
+	}
+
+	return `Your BMI is ${bmi.toFixed(2)} (${category})`;
+
+}
